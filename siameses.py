22446f7c27a4393e74039c17fc169f8b2494c994
Jill Palma Garro <77922190+jillpg@users.noreply.github.com>
2025-01-13 02:05:31 +0000

    siameses_functional

--- siameses.py
+++ siameses.py
@@ -4,26 +4,87 @@ import numpy as np
 import pandas as pd
 import pickle
 import os
+import sklearn
+import ast
+
+
+
 class OutfitRecommenderSiameses:
     def __init__(self, path_resources):
         print(os.getcwd())  # Obtiene el directorio actual
         self.path_resources = path_resources
         self.catalogo_path = path_resources + "catalogo_siameses.csv"
-        self.model_path = path_resources + "model_siameses.keras"
-        self.encoders_path = path_resources + "siameses_encoder.pkl"
+        self.model_path = path_resources + "siameses_base_model.weights.h5"
+        self.encoders_path = path_resources + "siameses_encoders.pkl"
         self.catalogo = self.load_catalogo()
         self.encoders = self.load_encoders()
         self.base_model = self.load_model()
 
+    def build_base_embedding_model(self):
+        # Inputs
+        image_input = tf.keras.Input(shape=(224,224,3), name="image_input")
+        attr_input = tf.keras.Input(shape=(6,), name="attr_input")
+
+        # Base ResNet50 preentrenada en ImageNet
+        # exclude top = True -> usaremos nuestras propias capas densas
+        base_resnet = tf.keras.applications.ResNet50(include_top=False, weights='imagenet', input_tensor=image_input, pooling='avg')
+        
+        # base_resnet.output tendrá el vector de features (2048)
+        image_embedding = tf.keras.layers.Dense(128, activation='relu')(base_resnet.output)
+
+        # Embeddings para atributos
+        gender_emb = tf.keras.layers.Embedding(input_dim=len(self.encoders["gender"].classes_), output_dim=4)(attr_input[:,0])
+        subcat_emb = tf.keras.layers.Embedding(input_dim=len(self.encoders["subCategory"].classes_), output_dim=4)(attr_input[:,1])
+        arttype_emb = tf.keras.layers.Embedding(input_dim=len(self.encoders["articleType"].classes_), output_dim=4)(attr_input[:,2])
+        season_emb = tf.keras.layers.Embedding(input_dim=len(self.encoders["season"].classes_), output_dim=4)(attr_input[:,3])
+        usage_emb = tf.keras.layers.Embedding(input_dim=len(self.encoders["usage"].classes_), output_dim=4)(attr_input[:,4])
+        color_emb = tf.keras.layers.Embedding(input_dim=len(self.encoders["Color"].classes_), output_dim=4)(attr_input[:,5])
+
+        # Concatenar embeddings de atributos
+        attr_concat = tf.keras.layers.Concatenate(axis=1)([
+            gender_emb,
+            subcat_emb,
+            arttype_emb,
+            season_emb,
+            usage_emb,
+            color_emb,
+        ])
+        attr_embedding = tf.keras.layers.Dense(32, activation='relu')(attr_concat)
+
+        # Fusionar atributos e imagen
+        combined = tf.keras.layers.concatenate([image_embedding, attr_embedding])
+        final_embedding = tf.keras.layers.Dense(128, activation=None)(combined)
+        final_embedding = tf.keras.layers.Lambda(
+            lambda t: tf.nn.l2_normalize(t, axis=1),
+            output_shape=(128,), name="l2_normalize"
+        )(final_embedding)
+        
+        return tf.keras.Model([*base_resnet.input, attr_input], final_embedding, name="base_embedding_model")
+
+
+
+
+
     def load_catalogo(self):
-        return pd.read_csv(self.catalogo_path)
+        KEYS = ["id", "gender", "subCategory", "articleType", "season", "usage", "Color", 'embedding']
+        # Función para convertir strings al formato de array
+        def parse_embedding(embedding_str):
+            # Remover corchetes y convertir los números separados por espacios a una lista de floats
+            return np.array([float(x) for x in embedding_str.strip("[]").split()])
+        # Aplicar la función a la columna de embeddings
+        catalogo= pd.read_csv(self.catalogo_path)
+        catalogo=catalogo[KEYS]
+        catalogo['embedding'] = catalogo['embedding'].apply(parse_embedding)
+        return catalogo
 
     def load_encoders(self):
         with open(self.encoders_path, 'rb') as file:
             return pickle.load(file)
 
     def load_model(self):
-        return tf.keras.models.load_model(self.model_path)
+        modelo=self.build_base_embedding_model()
+        modelo.load_weights(self.model_path)
+        return modelo
 
     def preprocess_input_resnet(self, img):
         """Normalización de imagen para ResNet50."""
@@ -43,21 +104,20 @@ class OutfitRecommenderSiameses:
         prenda_2 = prenda.copy()
         img = self.load_and_preprocess_image(image)
         img = tf.expand_dims(img, axis=0)  # Expande dimensión para el batch
-
         for col in self.encoders:
             prenda_2[col] = self.encoders[col].transform([prenda_2[col]])  # Transformación de atributos categóricos
         attrs = np.expand_dims(
-            np.array(prenda_2.drop(["id", "image"], axis=0), dtype=np.int32), axis=0
+            np.array(prenda_2.drop(["id", "image"], axis=0, errors='ignore'), dtype=np.int32), axis=0
         )
         emb = self.base_model.predict([img, attrs], verbose=0)
         return emb
 
-    def find_most_compatible(self, emb_ref):
+    def find_most_compatible(self, emb_ref, filtered_catalogo):
         """Encuentra la prenda más compatible con base en el embedding de referencia."""
-        embeddings_matrix = np.vstack(self.catalogo["embedding"])
+        embeddings_matrix = np.vstack(filtered_catalogo["embedding"])
         cosine_similarities = np.dot(embeddings_matrix, emb_ref.T)
         idx_max_similarity = np.argmax(cosine_similarities)
-        return self.catalogo.iloc[idx_max_similarity]
+        return filtered_catalogo.iloc[idx_max_similarity]
 
     def average_embeddings(self, embeddings):
         """Calcula el promedio de una lista de embeddings."""
@@ -78,12 +138,11 @@ class OutfitRecommenderSiameses:
 
         while True:
             # Lógica de categorías
-            if ("Bottomwear" in filter_categories or "Topwear" in filter_categories) and "Dress" not in filter_categories:
+            if "Dress" not in filter_categories and ("Bottomwear" in filter_categories or "Topwear" in filter_categories):
                 filter_categories.append("Dress")
             elif "Dress" in filter_categories and ("Topwear" not in filter_categories and  "Bottomwear" not in filter_categories):
                 filter_categories.append("Bottomwear")
                 filter_categories.append("Topwear")
-
             # Filtra el catálogo
             filtered_catalogo = self.filtrar_por_categoria(filter_categories)
 
@@ -92,7 +151,7 @@ class OutfitRecommenderSiameses:
                 break
 
             # Encuentra la prenda más compatible
-            selected = self.find_most_compatible(emb_outfit)
+            selected = self.find_most_compatible(emb_outfit,filtered_catalogo)
 
             # Actualiza el embedding del outfit
             emb_outfit = self.average_embeddings([emb_outfit, selected["embedding"]])
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,13 @@ from PIL import Image
 import pandas as pd
 import json
 from siameses import OutfitRecommenderSiameses
+import matplotlib.pyplot as plt
+import requests
+from io import BytesIO
+
 
 # Claves esperadas en el JSON
-EXPECTED_KEYS = {"id", "gender", "subCategory", "articleType", "season", "usage", "Color"}
+EXPECTED_KEYS = {"id", "gender", "subCategory", "articleType", "season", "usage", "Color" }
 
 # Título de la aplicación
 st.title("Cargar una imagen y un archivo JSON")
@@ -29,27 +33,55 @@ uploaded_json = st.file_uploader("Sube un archivo JSON con datos tabulares", typ
 if uploaded_json is not None:
     try:
         # Leer el archivo JSON
-        data = json.load(uploaded_json)
+        data_prenda = json.load(uploaded_json)
 
         # Verificar que el JSON es un diccionario (una sola instancia)
-        if not isinstance(data, dict):
+        if not isinstance(data_prenda, dict):
             st.error("El JSON debe contener una sola instancia representada como un diccionario.")
         else:
             # Verificar que contiene todas las claves esperadas
-            keys = set(data.keys())
+            keys = set(data_prenda.keys())
             missing_keys = EXPECTED_KEYS - keys
             if missing_keys:
                 st.error(f"El JSON no tiene las claves esperadas. Faltan: {', '.join(missing_keys)}")
             else:
                 # Convertir los datos a un DataFrame para visualización
-                df = pd.DataFrame([data])  # Convertir a una sola fila en DataFrame
+                df_data_prenda = pd.DataFrame([data_prenda])  # Convertir a una sola fila en DataFrame
                 # Mostrar el DataFrame
                 st.write("Contenido del JSON:")
-                st.dataframe(df)
+                st.dataframe(df_data_prenda)
     except Exception as e:
         st.error(f"Error al procesar el archivo JSON: {e}")
 
-siameses_model= OutfitRecommenderSiameses("/workspaces/TFG_OUTFITMATCH/resources/")
+
+if uploaded_image is not None and uploaded_json is not None:
+    path_resources="/workspaces/TFG_OUTFITMATCH/resources/"
+    siameses_model= OutfitRecommenderSiameses(path_resources)
+    outfit, _ = siameses_model.recommend_outfit(df_data_prenda.iloc[0].drop(["image"], axis=0),image)
+    df_outfit=pd.DataFrame(outfit)
+    
+
+    df_link=pd.read_csv(path_resources+"url_catalog.csv")
+    df_link['id'] = df_link['filename'].str.replace('.jpg', '', regex=False)
+    df_outfit['id'] = df_outfit['id'].astype(str)
+    df_link['id'] = df_link['id'].astype(str)
+    df_merged = pd.merge(df_outfit, df_link, on='id', how='inner')
+    st.dataframe(df_merged)
+    # Función para mostrar imágenes desde URLs
+    def mostrar_imagenes(df, url_column='link', max_images=4):
+        for i in range(min(len(df), max_images)):
+            try:
+                response = requests.get(df.iloc[i][url_column])
+                img = Image.open(BytesIO(response.content))
+                st.image(img, caption=f"Imagen {i+1}", use_container_width=True)
+            except Exception as e:
+                st.warning(f"No se pudo cargar la imagen {i+1}. Error: {e}")
+
+    # Llamar a la función para mostrar imágenes
+    st.title("Visualización de Outfit")
+    mostrar_imagenes(df_merged)
 
 
-
+#import tensorflow as tf
+#path="/workspaces/TFG_OUTFITMATCH/resources/encoder_combined_autoencoder.keras"
+#modelo_prueba=tf.keras.models.load_model(path)
